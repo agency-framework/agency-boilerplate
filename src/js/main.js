4cf5c76ab7ceb464a24304b8f5ad0beb148f19fc
Thorn Walli <thorn.walli@grabarzundpartner.de>
2016-11-04 15:04:39 +0100

    update parser call

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
 require('agency-pkg-service-animation-frame');
 require('agency-pkg-element-link/touch-indicator');
 
-var js = require('agency-pkg-service-parser').setup(require('./packages'));
+var js = require('agency-pkg-service-parser')(require('./packages'));
 
 (function(){
     $(function() {
