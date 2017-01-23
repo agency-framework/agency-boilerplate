d2be904535395414cd92779b51db3c7ab125a60a
Thorn Walli <lammpee@gmail.com>
2017-01-23 22:28:02 +0100

    heroku deploy build and start server heroku restart start only server

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,25 @@
 
 var options = require('minimist')(process.argv.slice(2));
 
+var fs = require('fs');
+var upath = require('upath');
 var gulp = require('gulp');
 var runSequence = require('run-sequence').use(gulp);
-require('agency-environment');
+var environment = require('agency-environment');
 require('agency-server');
 
 gulp.task('default', ['watch', 'server']);
 
 gulp.task('run', function(callback) {
-    if(!options.env || options.env === 'development' || options.env === 'package-development') {
+    if (options.heroku && fs.existsSync(upath.join(process.cwd(), environment.serverConfig.root))) {
+        runSequence('server', callback);
+    } else if (!options.env || options.env === 'development' || options.env === 'package-development') {
         runSequence('prebuild', 'default', callback);
     } else {
         runSequence('build', 'server', callback);
     }
 });
 
-process.once('SIGINT', function() { process.exit(0); });
+process.once('SIGINT', function() {
+    process.exit(0);
+});
